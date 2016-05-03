e55d63d1526dd325b0cdf9e6add15c5737c810c1
Waleed <deelaws89@gmail.com>
2016-05-02 22:35:22 -0700

    Add mod_auth controller file.

---
+++ MyWebsite/MyWebsite/mod_auth/controllers.py
@@ -0,0 +1,7 @@
+
+
+# Integrate with facebook o-auth
+
+# store auth-token from FB into database
+
+# create test accounts to ensure everything works: https://developers.facebook.com/apps/{YOUR_APP_ID}/roles/test-users/), 
