5fcfe80bf1c3103df90e61a4deae94a81f97cd9d
Waleed <deelaws89@gmail.com>
2016-07-03 22:11:39 -0700

    Basic script to add sample account.

---
+++ MyWebsite/create_sample_account.py
@@ -0,0 +1,18 @@
+﻿from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from MyWebsite.mod_auth.models import User
+from MyWebsite.mod_rescue.models import RescueAlert
+
+engine = create_engine('postgresql+psycopg2://postgres:qazwsx123@localhost/RescueApp')
+
+#connection = engine.connect()
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+new_user = User("deelaws@hotmail.com", "qwqwqwqwqw")
+
+session.add(new_user)
+
+session.commit()
