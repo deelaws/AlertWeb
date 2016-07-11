12ed15a6390bf76497cbb8fc5fd674db669b9513
Waleed <deelaws89@gmail.com>
2016-07-10 21:42:34 -0700

    Add record processor core class threading infrastructure

--- MyWebsite/record_processor.py
+++ MyWebsite/record_processor.py
@@ -18,20 +18,28 @@ class RecordProcessorCore():
         self.stop_extracting = False
         self.record_list_lock = Lock()
         self.record_pull_event = Event()
+        self.record_pull_event.clear()
         self.records = []
+        self.num_threads = num_threads
+        self.processor_threads = []
 
         # set the event so that when the record extractor thread is started,
         # it is signaled to pull the events.
         self.record_pull_event.set()
         self.engine = create_engine('postgresql+psycopg2://postgres:qazwsx123@localhost/RescueApp')
-        
-    
+
+    def shutdown(self):
+        self.stop_extracting = True
+
     def commence_engine(self): 
         self.SessionM = sessionmaker(bind=self.engine)
         self.session = self.SessionM()
 
-        # Start processing thread
+        # Start Extractor thread
         self.start_record_extractor_thread()
+
+        # Start Processing Threads
+        self.start_record_processing_threads()
 
     def record_extractor_runner(self):
         while(not self.stop_extracting):
@@ -39,19 +47,20 @@ class RecordProcessorCore():
                 print("waiting to pull more events")
 
             self.record_pull_event.wait()
-            print("Pulling events")
-            time_plus_twenty = get_time_plus_minutes(20)
-            alerts = self.session.query(RescueAlert).filter(RescueAlert.adventure_end_time > datetime.now(), RescueAlert.adventure_end_time < time_plus_twenty ).all()
-            self.record_list_lock.acquire()
-            try:
-                self.records[len(self.records): len(self.records)] = alerts
-                for al in self.records:
-                    print("\t Adventure Name is {}, end-time {}".format(al.adventure_name, al.adventure_end_time))
-            finally:
-                self.record_list_lock.release()
+
+            if self.shutdown:
+                break
+
+            self.pull_records_from_db()
             self.record_pull_event.clear()
 
-                
+    '''
+    Record processor method that works on the pulled records.
+    '''
+    def record_process_runner(self):
+        pass
+        
+
     '''
     Start's a thread that pulls records into a central record buffer.
     All record processing threads pull records from that record buffer.
@@ -60,32 +69,60 @@ class RecordProcessorCore():
         self.record_extractor_thread = Thread(target=self.record_extractor_runner)
         #self.record_extractor_thread.daemon = True
         self.record_extractor_thread.start()
-        
+        print("processing thread started\n")
 
     '''
     Starts the specified number of threads that process records.
     '''
     def start_record_processing_threads(self, num_threads):
+        for i in range(self.num_threads):
+            th = Thread(target=self.record_process_runner)
+            th.start()
+            self.processor_threads.append(th)
+    
+    '''
+    Pulls specified number(num) records from records buffer.
+    '''
+    def pull_records_from_mem(self, num):
         pass
 
     '''
-        pulls records from database
+    Pulls records from database
     '''
-    def pull_records(self):
+    def pull_records_from_db(self):
         self.record_list_lock.acquire()
         try:
             print("Pulling records from database")
+            
+            # pull records that will expire in 20 minutes
+            time_plus_twenty = get_time_plus_minutes(20)
+
+            # pull the records
+            alerts = self.session.query(RescueAlert).filter(RescueAlert.adventure_end_time > datetime.now(), RescueAlert.adventure_end_time < time_plus_twenty ).all()
+            self.records[len(self.records): len(self.records)] = alerts
+            for al in self.records:
+                print("\t Adventure Name is {}, end-time {}".format(al.adventure_name, al.adventure_end_time))
         finally:
             self.record_list_lock.release()
 
+    def join_process_threads(self):
+        for th in self.processor_threads:
+            th.join()
 
 if __name__ == "__main__":
     print("Starting Record Processor Engine")
     rpc =  RecordProcessorCore(1)
+    
     rpc.commence_engine()
-    rpc.record_extractor_thread.join()
+
+    
+    print("\t\t Engine commenced\n")
+    
     while("exit" != input("enter exit to quit\n")):
         print("\t Continuing to process records")
 
+    rpc.shutdown()
+    rpc.record_extractor_thread.join()
+    rpc.join_process_threads()
     print("Exiting processing")
     
