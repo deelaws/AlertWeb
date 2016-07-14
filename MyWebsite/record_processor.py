3e2a203b951345b3d5ccc83ce68fd5cb68671e5c
Waleed <deelaws89@gmail.com>
2016-07-13 23:14:50 -0700

    Basic alert mechanism has been built. We can now send an alert when a RESCue Alert expires.

--- MyWebsite/record_processor.py
+++ MyWebsite/record_processor.py
@@ -1,10 +1,11 @@
-﻿from sqlalchemy import create_engine
+﻿from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from MyWebsite.mod_auth.models import User
 from MyWebsite.mod_rescue.models import RescueAlert
 from datetime import datetime, timedelta
 from flask_mail import Mail
 from threading import Thread, Lock, Event
+from time import sleep
 
 def get_time_plus_minutes(min):
     now = datetime.now()
@@ -39,16 +40,18 @@ class RecordProcessorCore():
         self.start_record_extractor_thread()
 
         # Start Processing Threads
-        self.start_record_processing_threads()
+        self.start_record_processing_threads(self.num_threads)
 
     def record_extractor_runner(self):
+        print("record extractor runner started")
         while(not self.stop_extracting):
             if not self.record_pull_event.is_set():
                 print("waiting to pull more events")
 
             self.record_pull_event.wait()
 
-            if self.shutdown:
+            if self.stop_extracting:
+                print("gahhhhh")
                 break
 
             self.pull_records_from_db()
@@ -58,8 +61,30 @@ class RecordProcessorCore():
     Record processor method that works on the pulled records.
     '''
     def record_process_runner(self):
-        pass
-        
+        print("*** records process runner thread STARTED ***")
+        while(not self.stop_extracting):
+            records = self.pull_records_from_mem(3)
+            if None == records:
+                # If no records in the buffer then tell the extractor to pull
+                # records from memory
+                self.record_pull_event.set()
+            else:
+                print("*** processor has {} records to process ***".format(len(records)))
+                # Pull a couple records from the record buffer.
+                # What if the record buffer is empty
+                # recods are already sorted.
+                for alert in records:
+                    time_delta =  alert.adventure_end_time - datetime.now()
+                    
+                    # wait for (currenttime - first record) amount of time.
+                    print("\t sleeping for {} seconds".format(time_delta.seconds))
+                    if time_delta.days < 0:
+                        # time has expired. Send the alert now.
+                        print("\t TIME EXPIRED SENDING ALERT NOW.")
+                    else:
+                        sleep(time_delta.seconds)
+                        # wake up and send a message
+                        print("\t\t Sending email for alert which ended at {}".format(alert.adventure_end_time))
 
     '''
     Start's a thread that pulls records into a central record buffer.
@@ -82,21 +107,39 @@ class RecordProcessorCore():
     Pulls specified number(num) records from records buffer.
     '''
     def pull_records_from_mem(self, num):
-        pass
+        self.record_list_lock.acquire()
+        try:
+            if len(self.records) < num:
+                return None
+            else:
+                ret_list = []
+                for i in range(num):
+                    ret_list.append(self.records[i])
+
+                # remove the first *num* elements from record buffer
+                del self.records[:num]
+                
+                return ret_list
+        finally:
+            self.record_list_lock.release()
 
     '''
     Pulls records from database
     '''
     def pull_records_from_db(self):
+        print("pull records form db")
         self.record_list_lock.acquire()
         try:
             print("Pulling records from database")
             
             # pull records that will expire in 20 minutes
-            time_plus_twenty = get_time_plus_minutes(20)
+            # todo: make this configurable
+            time_plus_twenty = get_time_plus_minutes(10)
 
             # pull the records
-            alerts = self.session.query(RescueAlert).filter(RescueAlert.adventure_end_time > datetime.now(), RescueAlert.adventure_end_time < time_plus_twenty ).all()
+            alerts = self.session.query(RescueAlert).filter(RescueAlert.adventure_end_time > datetime.now(), RescueAlert.adventure_end_time < time_plus_twenty).order_by(asc(RescueAlert.adventure_end_time)).all()
+
+            # Essentially appending the records at the end.
             self.records[len(self.records): len(self.records)] = alerts
             for al in self.records:
                 print("\t Adventure Name is {}, end-time {}".format(al.adventure_name, al.adventure_end_time))
@@ -106,9 +149,13 @@ class RecordProcessorCore():
     '''
     Join with all the worker threads.
     '''
-    def join_process_threads(self):
+    def join_worker_threads(self):
+        self.record_extractor_thread.join()
+
         for th in self.processor_threads:
             th.join()
+
+
 
 if __name__ == "__main__":
     print("Starting Record Processor Engine")
@@ -119,8 +166,8 @@ if __name__ == "__main__":
     while("exit" != input("enter exit to quit\n")):
         print("\t Continuing to process records")
 
+    print("joinging on threads now")
     rpc.shutdown()
-    rpc.record_extractor_thread.join()
-    rpc.join_process_threads()
+    #rpc.join_worker_threads()
     print("Exiting processing")
     
