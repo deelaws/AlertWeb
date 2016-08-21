86ad5fce0e3026336da412d0ff1e736eda7197ec
Waleed <deelaws89@gmail.com>
2016-08-20 19:37:46 -0700

    try again.

--- alertwebstart.py
+++ alertwebstart.py
@@ -10,9 +10,9 @@ cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(insp
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
 
-#sys.path.insert(1, '.')
+sys.path.insert(1, '.')
 
-print(sys.path)
+#print(sys.path)
 
 from AlertWeb import *
 
