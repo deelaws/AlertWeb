0563bd4f4fdacd97ad1b58e50485dcc1bc65cb33
Waleed <deelaws89@gmail.com>
2016-08-20 19:09:34 -0700

    try to include mod from sub folder.

--- alertwebstart.py
+++ alertwebstart.py
@@ -3,7 +3,13 @@
 This script runs the AlertWeb application using a development server.
 """
 
-from os import environ
+import os, sys, inspect
+
+# use this if you want to include modules from a subfolder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"AlertWeb")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
 from AlertWeb import create_app
 
 if __name__ == '__main__':
