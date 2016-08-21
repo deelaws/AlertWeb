a457d3386b14b68ffed9b668c253fff9ed73311f
Waleed <deelaws89@gmail.com>
2016-08-20 20:05:40 -0700

    change port number

--- alertwebstart.py
+++ alertwebstart.py
@@ -20,7 +20,7 @@ print("calling main")
 
 print("outside main")
 app = create_app(os.environ.get('CONFIG_TYPE', 'production'))
-port = int(os.environ.get('PORT', 33447))
+port = int(os.environ.get('PORT', 33507))
 print(port)
 app.run(debug=False, host='0.0.0.0', port=port)
 print(sys.path)
